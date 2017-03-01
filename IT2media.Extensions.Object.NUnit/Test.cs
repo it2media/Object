147a005bc4ad06e912921f0c19c0eefb97237093
Markus Malz <asgart79@gmail.com>
2017-03-01 16:00:32 +0100

    Dump props and IEnumerable

---
+++ IT2media.Extensions.Object.NUnit/Test.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace IT2media.Extensions.Object.NUnit
+{
+	[TestFixture()]
+	public class Test
+	{
+		public class MyPropertyTestClass
+		{
+			public string MyProperty
+			{
+				get;
+				set;
+			} = "MyPropertyTest2";
+
+			public int MyProperty2
+			{
+				get;
+				set;
+			} = 4711;
+		} 
+
+		[Test()]
+		public void TestCase()
+		{
+			List<string> list = new List<string>();
+
+			list.Add("test1");
+			list.Add("test2");
+
+			list.Dump();
+
+			MyPropertyTestClass testClass = new MyPropertyTestClass();
+
+			testClass.Dump();
+		}
+	}
+}
---
+++ IT2media.Extensions.Object/ObjectExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace IT2media.Extensions.Object
+{
+	public class Test : IEnumerable
+	{
+		public IEnumerator GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public static class ObjectExtensions
+	{
+		public static string Dump(this object o)
+		{
+			string s = o.ToString();
+
+			Type t = o.GetType();
+
+			var propInfos = t.GetRuntimeProperties();
+
+			try
+			{
+				foreach (var propInfo in propInfos)
+				{
+					var m = propInfo.DeclaringType.GetTypeInfo().DeclaredMethods;
+
+					var x = (from y in m where y.Name == "GetEnumerator" select y);
+					if (x.Any())
+					{
+						continue;
+					}
+
+					Debug.WriteLine(propInfo.Name + ":" + propInfo.GetValue(o));
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+
+			if (o as IEnumerable != null)
+			{
+				foreach (var sub in o as IEnumerable)
+				{
+					Debug.WriteLine(sub);
+				}
+			}
+			else
+			{
+				Debug.WriteLine(s);
+			}
+
+			return s;
+		}
+	}
+}
