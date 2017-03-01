bec64d4a7f319d4250ddbd3ef4606ce7ec1eda78
Markus Malz <asgart79@gmail.com>
2017-03-01 16:38:52 +0100

    Prepare for nuget

--- IT2media.Extensions.Object/ObjectExtensions.cs
+++ IT2media.Extensions.Object/ObjectExtensions.cs
@@ -7,71 +7,21 @@ using Newtonsoft.Json;
 
 namespace IT2media.Extensions.Object
 {
-	public class Test : IEnumerable
-	{
-		public IEnumerator GetEnumerator()
-		{
-			throw new NotImplementedException();
-		}
-	}
-
 	public static class ObjectExtensions
 	{
-		public static T Dump<T>(this T o)
+		public static T Dump<T>(this T obj)
 		{
 			try
 			{
-				string json = JsonConvert.SerializeObject(o, Formatting.Indented);
+				string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
 				Debug.WriteLine(json);
 			}
 			catch (Exception ex)
 			{
 				Debug.WriteLine(ex);
 			}
-			return o;
-		}
 
-		public static string Dump2(this object o)
-		{
-			string s = o.ToString();
-
-			Type t = o.GetType();
-
-			var propInfos = t.GetRuntimeProperties();
-
-			try
-			{
-				foreach (var propInfo in propInfos)
-				{
-					var m = propInfo.DeclaringType.GetTypeInfo().DeclaredMethods;
-
-					var x = (from y in m where y.Name == "GetEnumerator" select y);
-					if (x.Any())
-					{
-						continue;
-					}
-
-					Debug.WriteLine(propInfo.Name + ":" + propInfo.GetValue(o));
-				}
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine(ex);
-			}
-
-			if (o as IEnumerable != null)
-			{
-				foreach (var sub in o as IEnumerable)
-				{
-					Debug.WriteLine(sub);
-				}
-			}
-			else
-			{
-				Debug.WriteLine(s);
-			}
-
-			return s;
+			return obj;
 		}
 	}
 }
