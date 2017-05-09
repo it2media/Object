823088ad00d41275f76392d0c2558301d646e5a1
malzma <markus.malz@it2media.de>
2017-05-09 09:39:56 +0200

    xUnit Test

---
+++ IT2media.Extensions.Object.Test/ObjectExtensionsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IT2media.Extensions.Object.Test
+{
+    public class ObjectExtensionsTest
+    {
+        [Fact]
+        public void DumpToFileTest()
+        {
+            List<string> list = new List<string>();
+
+            list.Add("item1");
+            list.Add("item2");
+
+            list.Dump();
+
+            SampleClass instance = new SampleClass();
+
+            instance.Dump();
+
+            var file = instance.DumpToFileAsync("sampleClass.json").Result;
+
+            string fileContent = System.IO.File.ReadAllText(file.Path);
+
+            Console.WriteLine(file.Path);
+
+            Assert.True(!string.IsNullOrWhiteSpace(fileContent));
+        }
+    }
+}
---
+++ IT2media.Extensions.Object.Test/SampleClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT2media.Extensions.Object.Test
+{
+    public class SampleClass
+    {
+        public string StringProperty
+        {
+            get;
+            set;
+        }
+
+        public int IntProperty
+        {
+            get;
+            set;
+        }
+    }
+}
